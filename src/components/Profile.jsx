9ccaedb32a648fa18c40ac049bc60bc31c0f769b
Wladyslaw <kvazid@gmail.com>
2021-01-24 17:35:32 +0700

    install react + create base components

---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src="http://www.passagemepassaporte.com/wp-content/uploads/2016/02/jamaica-3.jpg" />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            my posts
+            <div>
+                new post
+            </div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
