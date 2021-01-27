575193f88c143a5be6cbb8f28c049991d84cdf75
Wladyslaw <kvazid@gmail.com>
2021-01-27 23:20:54 +0700

    20: добавил NavLink ссылкам в навигации

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,26 @@
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
 const Navbar = () => {
-  return <nav className={s.nav}>
-    <div className={s.item}>
-      <a href="/profile">Profile</a>
-    </div>
-    <div className={`${s.item} ${s.active}`}>
-      <a href='/dialogs'>Messages</a>
-    </div>
-    <div className={s.item}>
-      <a>News</a>
-    </div>
-    <div className={s.item}>
-      <a>Music</a>
-    </div>
-    <div className={s.item}>
-      <a>Settings</a>
-    </div>
-  </nav>
+  return (
+    <nav className={s.nav}>
+      <div className={s.item}>
+        <NavLink to="/profile" activeClassName={s.active}>Profile</NavLink>
+      </div>
+      <div className={`${s.item} ${s.active}`}>
+        <NavLink to='/dialogs' activeClassName={s.active}>Messages</NavLink>
+      </div>
+      <div className={s.item}>
+        <a>News</a>
+      </div>
+      <div className={s.item}>
+        <a>Music</a>
+      </div>
+      <div className={s.item}>
+        <a>Settings</a>
+      </div>
+    </nav>
+  )
 }
 
 export default Navbar;
