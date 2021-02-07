54a8159ed569ee90557036003b99f44c95fb087a
Wladyslaw <kvazid@gmail.com>
2021-02-08 00:16:46 +0700

    32: создал функцию в state.js, которая будет менять state

--- src/redux/state.js
+++ src/redux/state.js
@@ -27,4 +27,14 @@ let state = {
     }
 }
 
+export let addPost = (postMessage) => {
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likesCount: 0
+    }
+
+    state.profilePage.posts.push(newPost);
+}
+
 export default state;
