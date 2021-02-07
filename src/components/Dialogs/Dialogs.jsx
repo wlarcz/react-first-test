675d75a59f45208f6794949123b27db8da77ae06
Wladyslaw <kvazid@gmail.com>
2021-02-07 22:31:40 +0700

    30...31: добавил обработчик событий на кнопки отправки сообщений и постов. onClick, ref, React.createRef

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -2,11 +2,17 @@ import { NavLink } from 'react-router-dom';
 import DialogItem from './DialogItem/DialogItem';
 import s from './Dialogs.module.css';
 import Message from './Message/Message';
+import React, { Component } from 'react';
 
 const Dialogs = (props) => {
 
     let dialogsElements = props.state.dialogs.map( d => <DialogItem name={ d.name } id={ d.id } /> )
     let messagesElements = props.state.messages.map( m => <Message message={ m.message } /> )
+
+    let newMessageElement = React.createRef();
+    let sendMessage = () => {
+        alert(newMessageElement.current.value);
+    }
 
     return (
         <div className={s.dialogs}>
@@ -16,7 +22,17 @@ const Dialogs = (props) => {
 
             <div className={s.messages}>
                 { messagesElements }
+                <div>
+                    <div>
+                        <textarea cols="15" rows="3" ref={ newMessageElement } ></textarea>
+                    </div>
+                    <div>
+                        <button onClick={ sendMessage }> OK </button>
+                    </div>
+                </div>
             </div>
+
+            
 
         </div>
     )
