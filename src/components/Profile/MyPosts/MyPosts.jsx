d21a729b8b5ede70d803373a89f533d3c1f75e41
Wladyslaw <kvazid@gmail.com>
2021-02-13 21:11:05 +0700

    39: создал actionCreator в state.js, чтоб UI не создавал функции

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,6 +1,7 @@
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 import React, { Component } from 'react';
+import { addPostActionCreator, updeteNewPostTextActionCreator } from '../../../redux/state';
 
 const MyPosts = (props) => { 
     let postsElements = props.posts.map( p => <Post message={p.message} likesCount={p.likesCount} />);
@@ -8,12 +9,12 @@ const MyPosts = (props) => {
     let newPostElement = React.createRef();
 
     let addPost = () => {
-        props.dispatch( { type: 'ADD-POST'} );
+        props.dispatch( addPostActionCreator() );
     }
 
     let onPostChange = () => {
         let text = newPostElement.current.value;
-        let action = { type: 'UPDATE-NEW-POST-TEXT', newText: text };
+        let action = updeteNewPostTextActionCreator(text);
         props.dispatch( action ); 
         // props.dispatch( { type: 'UPDATE-NEW-POST-TEXT', newText: text } );
     }
