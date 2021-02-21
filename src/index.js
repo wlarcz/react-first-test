2601549c72d8b991ff8ce16e6e4083be79c836d6
Wladyslaw <kvazid@gmail.com>
2021-02-22 01:14:37 +0700

    46..47: сделал копию состояния в PrifileReducer и DialogReducer

--- src/index.js
+++ src/index.js
@@ -7,22 +7,15 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-let rerenderEntireTree = (state) => {
-  ReactDOM.render(
-    <BrowserRouter>
-      <Provider store={store} >
-        <App />
-      </Provider>
-    </BrowserRouter>,
-    document.getElementById('root')
-  );
-}
-rerenderEntireTree(store.getState());
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store} >
+      <App />
+    </Provider>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
 
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntireTree(state);
-});
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
