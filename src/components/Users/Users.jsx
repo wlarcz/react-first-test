16ef1fa0822a8735433a61c84bde1f51bd9e07d9
Wladyslaw <kvazid@gmail.com>
2021-03-02 18:59:26 +0700

    54: поместил AJAX-запрос в componentDidMount

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,16 +4,11 @@ import styles from './users.module.css';
 import userPhoto from '../../assets/images/user.jpg';
 
 class Users extends React.Component {
-
-    constructor(props) {
-        super(props);
-        if (this.props.users.length === 0) {
-
-            axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
                 debugger;
                 this.props.setUsers(response.data.items);
             })
-        }
     }
 
     render() {
