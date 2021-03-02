0fa6d2efb10481ae89f87916cd7f87b94b9417a5
Wladyslaw <kvazid@gmail.com>
2021-03-03 00:49:44 +0700

    55: реализовал pagination, постраничный вывод пользователей

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -5,14 +5,31 @@ import userPhoto from '../../assets/images/user.jpg';
 
 class Users extends React.Component {
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-                debugger;
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&counter=${this.props.pageSize}`).then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            })
+    }
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&counter=${this.props.pageSize}`).then(response => {
                 this.props.setUsers(response.data.items);
             })
     }
-
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return <div>
+            {pages.map( (p)=> {
+                return <span className={this.props.currentPage === p && styles.selectedPage} onClick={ (e) => {  this.onPageChanged(p)} }>{ p }</span>
+            }
+
+            )}
             {
                 this.props.users.map(u => <div key={u.id}>
                     <span>
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -1,13 +1,14 @@
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT = 'SET_TOTAL_USERS_COUNT';
 
 let initialState = {
-    users: [
-        // {id: 1, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg', followed: true, fullName: 'Wladislaw', status: 'I am a boss', location: {city: 'Tomsk', country: 'Russia'}},
-        // {id: 2, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg', followed: false, fullName: 'Petrushka', status: 'I want to sleep', location: {city: 'Minsk', country: 'Belarus'}},
-        // {id: 3, photoUrl: 'https://upload.wikimedia.org/wikipedia/commons/1/1c/Dmitry_Nagiev_2017_3.jpg', followed: true, fullName: 'Taras', status: 'relax', location: {city: 'Kiev', country: 'Ukraine'}},
-    ]
+    users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage: 2
 }
 
 
@@ -34,6 +35,10 @@ const usersReducer = (state = initialState, action) => {
         case SET_USERS:
             // return { ...state, users: [...state.users, ...action.users]};
             return { ...state, users: [...action.users]};
+        case SET_CURRENT_PAGE:
+            return { ...state, currentPage: action.currentPage};
+        case SET_TOTAL_USERS_COUNT:
+            return { ...state, totalUsersCount: action.count};
         default:
             return state;
     }
@@ -42,5 +47,7 @@ const usersReducer = (state = initialState, action) => {
 export const followAC = (userID) => ({ type: FOLLOW, userID })
 export const unfollowAC = (userID) => ({ type: UNFOLLOW, userID })
 export const setUsersAC = (users) => ({ type: SET_USERS, users })
+export const setCurrentPageAC = (currentPage) => ({ type: SET_CURRENT_PAGE, currentPage })
+export const setUsersTotalCountAC = (totalUsersCount) => ({ type: SET_TOTAL_USERS_COUNT, count: totalUsersCount })
 
 export default usersReducer;
