8376034c6b6288645c697869258e2d3eaf92aa10
Wladyslaw <kvazid@gmail.com>
2021-03-12 10:17:41 +0700

    61: login, auth/me отображение логина в хедере при авторизации

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { BrowserRouter, Route } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from './components/Users/UsersContainer';
 import ProfileContainer from './components/Profile/ProfileContainer';
+import HeaderContainter from './components/Header/HeaderContainer';
 
 const App = (props) => {
   
   return (
       <div className='app-wrapper'>
-        <Header />
+        <HeaderContainter />
         <Navbar />
         <div className='app-wrapper-content'>
           <Route path='/dialogs' render={ () => <DialogsContainer /> }/>
