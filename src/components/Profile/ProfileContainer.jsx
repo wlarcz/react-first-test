99bd432beb33cfeb4946663aeb5d9b8e0d81321f
Wladyslaw <kvazid@gmail.com>
2021-03-18 00:53:27 +0700

    67: добавил thunk для profile и header

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import Profile from './Profile';
-import * as axios from 'axios';
 import { connect } from 'react-redux';
-import { setUserProfile } from '../../redux/profile-reducer';
+import { getUserProfile } from '../../redux/profile-reducer';
 import { withRouter } from 'react-router';
+
 
 class ProfileContainer extends React.Component {
 
@@ -12,10 +12,7 @@ class ProfileContainer extends React.Component {
         if (!userId) {
             userId = 2;
         }
-        
-        axios.get(`https://social-network.samuraijs.com/api/1.0/profile/` + userId).then(response => {
-            this.props.setUserProfile(response.data);
-        })
+        this.props.getUserProfile(userId);
     }
 
     render() {
@@ -30,4 +27,4 @@ let mapStateToProps = (state) => ({
 })
 
 let WithUrlDataContainerComponent =  withRouter(ProfileContainer);
-export default connect(mapStateToProps, {setUserProfile}) (WithUrlDataContainerComponent);
+export default connect(mapStateToProps, {getUserProfile}) (WithUrlDataContainerComponent);
