c65947255b4009252bb746d0f8241cb4f09f2f4e
Wladyslaw <kvazid@gmail.com>
2021-03-21 22:20:21 +0700

    70: функция compose, - конвеер для редиректа dialogs, profile, usersContainer

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -6,6 +6,7 @@ import Users from './Users';
 import Preloader from "../common/Preloader/Preloader";
 import { usersAPI } from "../../api/api";
 import { withAuthRedirect } from "../../hoc/withAuthRedirect";
+import { compose } from "redux";
 
 class UsersContainer extends React.Component {
     componentDidMount() {
@@ -46,9 +47,7 @@ let mapStateToProps = (state) => {
     }
 }
 
-
-
-export default withAuthRedirect(connect(mapStateToProps,
-    {
-        follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers
-    })(UsersContainer));
+export default compose(
+    withAuthRedirect,
+    connect(mapStateToProps, { follow, unfollow, setCurrentPage, toggleFollowingProgress, getUsers })
+) (UsersContainer);
