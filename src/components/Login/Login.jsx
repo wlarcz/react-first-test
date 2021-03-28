5128e1e9d323c57ac1a741b8dca22229ef5ea2f7
Wladyslaw <kvazid@gmail.com>
2021-03-28 22:08:51 +0700

    75.0: переверстал компоненту login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,27 @@
+const LoginForm = (props) => {
+    return (
+        <form action="">
+            <div>
+                <input placeholder={'Login'} />
+            </div>
+            <div>
+                <input placeholder={'Password'} />
+            </div>
+            <div>
+                <input type={'checkbox'} /> remember me
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
 const Login = (props) => {
-    return <h1>LOGIN</h1>
+    return <div>
+        <h1>Login</h1>
+        <LoginForm />
+    </div>
 }
 
 export default Login;
