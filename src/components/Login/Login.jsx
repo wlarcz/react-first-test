8f71cdf304e378523cf3b9714a525b10625ce646
Wladyslaw <kvazid@gmail.com>
2021-03-29 21:44:55 +0700

    75.1: установил formik, по тутору 10 vid... валидация полей и ошибки незаполненых полей

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,17 +1,71 @@
+import { Formik, useFormik } from 'formik';
+import s from './Login.module.css';
+
+const initialValues = {
+    login: '',
+    password: '',
+    rememberMe: false
+}
+
+const onSubmit = values => {
+    console.log('Form data', values)
+}
+
+const validate = values => {
+    let errors = {}
+
+    if (!values.login) {
+        errors.login = 'Required'
+    }
+
+    if (!values.password) {
+        errors.password = 'Required'
+    }
+
+    return errors
+}
+
 const LoginForm = (props) => {
+    
+    const formik = useFormik({
+        initialValues,
+        onSubmit,
+        validate
+    })
+
+    console.log('Visited fields', formik.touched);
+
     return (
-        <form action="">
-            <div>
-                <input placeholder={'Login'} />
+        <form onSubmit={formik.handleSubmit} >
+            <div className={s.formControl} >
+                <input
+                placeholder={'Login'}
+                name={'login'} 
+                onChange={formik.handleChange} 
+                onBlur={formik.handleBlur}
+                value={formik.values.login}
+                 />
+                 { formik.touched.login && formik.errors.login ? <div className={ s.error } >{ formik.errors.login }</div> : null }
+            </div>
+            <div className={s.formControl} >
+                <input 
+                placeholder={'Password'} 
+                name={'password'}
+                type={'password'} 
+                onChange={formik.handleChange} 
+                onBlur={formik.handleBlur}
+                value={formik.values.password} 
+                />
+                { formik.touched.password && formik.errors.password ? <div className={ s.error } >{ formik.errors.password }</div> : null }
             </div>
             <div>
-                <input placeholder={'Password'} />
+                <input type={'checkbox'}
+                name={'rememberMe'}
+                value={formik.values.rememberMe} 
+                /> remember me
             </div>
             <div>
-                <input type={'checkbox'} /> remember me
-            </div>
-            <div>
-                <button>Login</button>
+                <button type={'submit'} >Login</button>
             </div>
         </form>
     )
