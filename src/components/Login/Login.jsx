f8b47192d5653930362380c73525ae7ee9794c44
Wladyslaw <kvazid@gmail.com>
2021-04-04 13:47:05 +0700

    75.3: login-form v2

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,57 +1,74 @@
+import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import s from './Login.module.css';
 import * as Yup from 'yup';
 
-const initialValues = {
-    login: '',
-    password: '',
-    rememberMe: false
+const TextError = (props) => {
+    return (
+        <div className={s.error}>
+            {props.children}
+        </div>
+    )
 }
 
-const onSubmit = values => {
-    console.log('Form data', values)
+const Input = (props) => {
+    const { label, name, ...rest} = props
+    return (
+        <div className={ s.formControl }>
+            <label htmlFor={name}>{label}</label>
+            <Field id={name} name={name} {...rest} />
+            <ErrorMessage name={name} component={TextError} />
+        </div>
+    )
 }
 
-const validationSchema = Yup.object({
-    login: Yup.string().required('Обизатильна!'),
-    password: Yup.string().required('Важно!')
-})
+const FormikControl = (props) => {
+    const { control, ...rest } = props
+    switch(control) {
+        case 'input': return <Input {...rest} />
+        case 'textarea':
+        case 'select':
+        case 'radio':
+        case 'checkbox':
+        case 'date':
+        default: return null
+    }
+}
 
 const LoginForm = (props) => {
+    const initialValues = {
+        email: '',
+        password: ''
+    }
 
+    const validationSchema = Yup.object({
+        email: Yup.string().email('Invalid email format').required('Обязательно'),
+        password: Yup.string().required('Обязательно')
+    })
 
-    // console.log('Visited fields', formik.touched);
-
+    const onSubmit = values => {
+        console.log('Form data', values);
+    }
     return (
-        <Formik
-        initialValues={ initialValues }
-        validationSchema={ validationSchema }
-        onSubmit={ onSubmit }>
-            <Form>
-                <div className={s.formControl} >
-                    <Field
-                        placeholder={'Login'}
-                        name={'login'}
-                    />
-                    <ErrorMessage name='login' />
-                </div>
-                <div className={s.formControl} >
-                    <Field
-                        placeholder={'Password'}
-                        name={'password'}
-                        type={'password'}
-                    />
-                    <ErrorMessage name='password' />
-                </div>
-                <div>
-                    <Field type={'checkbox'}
-                        name={'rememberMe'}
-                    /> remember me
-            </div>
-                <div>
-                    <button type={'submit'} >Login</button>
-                </div>
-            </Form>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+            {
+                formik => {
+                    return <Form>
+                        <FormikControl
+                        control='input'
+                        type='email'
+                        label='Email'
+                        name='email' />
+                        
+                        <FormikControl
+                        control='input'
+                        type='password'
+                        label='Password'
+                        name='password' />
+                        <button type='submit' disabled={!formik.isValid} >Submit</button>
+                    </Form>
+                }
+            }
         </Formik>
     )
 }
