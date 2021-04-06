3e5b9639ca73682e50c0c08ef2cbbc6f214856e9
Wladyslaw <kvazid@gmail.com>
2021-04-06 22:05:58 +0700

    75.4: login-form-final - next lesson

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -12,24 +12,40 @@ const TextError = (props) => {
 }
 
 const Input = (props) => {
-    const { label, name, ...rest} = props
+    const { label, name, ...rest } = props
     return (
-        <div className={ s.formControl }>
+        <div className={s.formControl}>
             <label htmlFor={name}>{label}</label>
             <Field id={name} name={name} {...rest} />
             <ErrorMessage name={name} component={TextError} />
         </div>
     )
 }
+const RememberMe = (props) => {
+    const { label, name, ...rest } = props
+    return (
+        <div className={s.formControl}>
+            <label htmlFor={name}>{label}</label>
+            <Field id={name} name={name} {...rest} />
+        </div>
+    )
+}
+
+{/* <div>
+    <input type={'checkbox'}
+        name={'rememberMe'}
+        value={initialValues.rememberMe}
+    /> remember me
+</div> */}
 
 const FormikControl = (props) => {
     const { control, ...rest } = props
-    switch(control) {
+    switch (control) {
         case 'input': return <Input {...rest} />
         case 'textarea':
         case 'select':
         case 'radio':
-        case 'checkbox':
+        case 'checkbox': return <RememberMe {...rest} />
         case 'date':
         default: return null
     }
@@ -38,33 +54,52 @@ const FormikControl = (props) => {
 const LoginForm = (props) => {
     const initialValues = {
         email: '',
-        password: ''
+        password: '',
+        rememberMe: false
     }
 
+    // const checkboxOption = {
+    //     key: 'Option 1',
+    //     value: 'rOption 1'
+    // }
     const validationSchema = Yup.object({
         email: Yup.string().email('Invalid email format').required('Обязательно'),
         password: Yup.string().required('Обязательно')
     })
 
-    const onSubmit = values => {
-        console.log('Form data', values);
-    }
+    // const onSubmit = values => {
+    //     console.log('Form data', values);
+    // }
+    // const onSubmit = (values, { setSubmitting }) => {
+    //     setTimeout(() => {
+    //       alert(JSON.stringify(values, null, 2));
+    //       setSubmitting(false);
+    //     }, 400);
+    //   }
+
     return (
-        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+        <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={props.onSubmit}>
             {
                 formik => {
                     return <Form>
                         <FormikControl
-                        control='input'
-                        type='email'
-                        label='Email'
-                        name='email' />
+                            control='input'
+                            type='email'
+                            label='Email'
+                            name='email' />
+
+                        <FormikControl
+                            control='input'
+                            type='password'
+                            label='Password'
+                            name='password' />
                         
                         <FormikControl
-                        control='input'
-                        type='password'
-                        label='Password'
-                        name='password' />
+                            control='checkbox'
+                            type='checkbox'
+                            label='Remember me'
+                            name='rememberMe' />
+
                         <button type='submit' disabled={!formik.isValid} >Submit</button>
                     </Form>
                 }
@@ -74,9 +109,16 @@ const LoginForm = (props) => {
 }
 
 const Login = (props) => {
+    const onSubmit = (values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }
+
     return <div>
         <h1>Login</h1>
-        <LoginForm />
+        <LoginForm onSubmit={ onSubmit } />
     </div>
 }
 
