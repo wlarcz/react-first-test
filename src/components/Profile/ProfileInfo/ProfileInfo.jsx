07bf1afaa9af0ec020247d2924dd6638978acb69
Wladyslaw <kvazid@gmail.com>
2021-05-06 20:00:38 +0700

    84: hook, useState, хуки

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import Preloader from '../../common/Preloader/Preloader';
 import s from './ProfileInfo.module.css';
 import ProfileStatus from './ProfileStatus';
+import ProfileStatusWithHooks from './ProfileStatusWithHooks';
 
 const ProfileInfo = (props) => {
     if (!props.profile) {
@@ -13,7 +14,7 @@ const ProfileInfo = (props) => {
             </div> */}
             <div className={s.descriptionBlock}>
                 <img src={props.profile.photos.large} />
-                <ProfileStatus status={ props.status } updateStatus={ props.updateStatus } />
+                <ProfileStatusWithHooks status={ props.status } updateStatus={ props.updateStatus } />
             </div>
         </div>
     )
