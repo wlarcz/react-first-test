1a890c6251e095c1ed4b2d0e2f6f13549b8d6e67
Wladyslaw <kvazid@gmail.com>
2021-06-05 23:19:23 +0700

    94: React.lazy и React.Suspense

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import { Component } from 'react';
+import React, { Component, Suspense } from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import { Route, withRouter } from 'react-router-dom';
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from './components/Users/UsersContainer';
-import ProfileContainer from './components/Profile/ProfileContainer';
 import HeaderContainter from './components/Header/HeaderContainer';
 import LoginPage from './components/Login/Login';
 import { connect, Provider } from "react-redux";
@@ -13,7 +11,10 @@ import { initializeApp } from './redux/app-reducer';
 import Preloader from './components/common/Preloader/Preloader';
 import store from './redux/redux-store';
 import { BrowserRouter } from 'react-router-dom';
+import { withSuspense } from './hoc/withSuspense';
 
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class App extends Component {
   componentDidMount() {
@@ -30,9 +31,13 @@ class App extends Component {
         <HeaderContainter />
         <Navbar />
         <div className='app-wrapper-content'>
-
+        {/* <Suspense fallback={<div>Загрузка...</div>}>
           <Route path='/dialogs' render={() => <DialogsContainer />} />
           <Route path='/profile/:userId?' render={() => <ProfileContainer />} />
+        </Suspense> */}
+          <Route path='/dialogs' render={withSuspense(DialogsContainer)} />
+          <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)} />
+          
           <Route path='/users' render={() => <UsersContainer />} />
           <Route path='/login' render={() => <LoginPage />} />
 
